e48a81ff75fdb58e8da8d77d6628d98b462aaf54
Jioh L. Jung <ziozzang@gmail.com>
2020-08-25 22:50:55 +0900

    Update 0.prepare.sh

--- 0.prepare.sh
+++ 0.prepare.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+TAGS=${TAGS:-"latest"}
+
 mkdir -p docker/
 
 # Prepare admin tools.
 docker build -t systools systools/
 docker save systools | gzip > docker/systools.tgz
+
+docker pull gitlab/gitlab-ce:${TAGS}
+docker save gitlab/gitlab-ce:${TAGS} | gzip > docker/gitlab-ce.tgz
 
 # set docker-compose.yml
